e0837dc7e1312dda07b375ef57c3d1fd8be6242a
LAL102LAL <bobychs66@gmail.com>
2024-07-05 03:32:46 +0530

    Updated HTML and JS files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById('joinButton').addEventListener('click', function() {
-    alert('Redirecting to Telegram Channel');
-    window.location.href = 'https://t.me/joinchat/XXXXXX'; // replace with your Telegram channel link
+   
+    window.location.href = 'https://t.me/cricketsession_matchtips'; // replace with your Telegram channel link
 });
