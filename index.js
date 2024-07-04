a68a8649065f00119ad44794b9d9c8c3fe11aa7a
LAL102LAL <bobychs66@gmail.com>
2024-07-05 03:15:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('joinButton').addEventListener('click', function() {
+    alert('Redirecting to Telegram Channel');
+    window.location.href = 'https://t.me/joinchat/XXXXXX'; // replace with your Telegram channel link
+});
